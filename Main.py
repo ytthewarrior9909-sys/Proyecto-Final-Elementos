7b207d7777a85775564b8e74185c1be915069e5c
Carlo <ytthewarrior9909@gmail.com>
2026-05-21 21:07:29 -0600

    menu

---
+++ Main.py
@@ -0,0 +1,16 @@
+
+opcion = int(input('Digite la opcion deseada: ')
+print("=========== PYTHONIA RPG ===========")
+print("1. Registrar aventurero")
+print("2. Ver aventureros")
+print("3. Buscar aventurero")
+print("4. Entrenar aventurero")
+print("5. Tienda")
+print("6. Inventario")
+print("7. Batallas")
+print("8. Estadísticas")
+print("9. Ranking")
+print("0. Salir")
+print('====================================')\
+while opcion != 0:
+    
