221793c899acb3110e449d7ac483564539f106e6
Carlo <ytthewarrior9909@gmail.com>
2026-05-21 23:36:22 -0600

    Inicio registro y datos y creacion de txt

---
+++ datos.py
@@ -0,0 +1,24 @@
+nombres = []
+clases = []
+niveles = []
+vidas = []
+ataques = []
+defensas = []
+oros = []
+estados = []
+inventarios = []
+# Stats base por clase
+VIDA_GUERRERO = 150
+ATAQUE_GUERRERO = 20
+DEFENSA_GUERRERO = 15
+
+VIDA_MAGO = 80
+ATAQUE_MAGO = 35
+DEFENSA_MAGO = 5
+
+VIDA_ARQUERO = 100
+ATAQUE_ARQUERO = 25
+DEFENSA_ARQUERO = 10
+
+ORO_INICIAL = 100
+NIVEL_INICIAL = 1
---
+++ registro.py
@@ -0,0 +1,7 @@
+from datos import nombres, clases, niveles, vidas, ataques, defensas, oros, estados, inventarios
+from datos import VIDA_GUERRERO, ATAQUE_GUERRERO, DEFENSA_GUERRERO
+from datos import VIDA_MAGO, ATAQUE_MAGO, DEFENSA_MAGO
+from datos import VIDA_ARQUERO, ATAQUE_ARQUERO, DEFENSA_ARQUERO
+from datos import ORO_INICIAL, NIVEL_INICIAL
+def registrar_aventurero():
+    aventurero = int(input("Ingrese el nombre del aventurero: "))
