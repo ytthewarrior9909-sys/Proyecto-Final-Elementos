443a8ae174b42ea3b42c8bf9aabbcac490910bcb
Carlo <ytthewarrior9909@gmail.com>
2026-05-29 00:18:16 -0600

    entrenamiento

---
+++ entrenamiento.py
@@ -0,0 +1,19 @@
+def entrenamiento_aventureros():
+    print('-------OPCIONES DE ENTRENAMIENTO---------')
+    print('1: Entrenar ataque, +5 de ataque -10 de oro:')
+    print('2: Entrenar defensa, +3 de defensa -10 de oro:')
+    print('3: Entrenar vida, +20 vida -50 de oro:')
+    print('4: Subir de nivel, +1 nivel -100 de oro')
+    print('5: Salir de la tienda')
+    opcionEntrenamineto = int(input("Ingrese el numero de opcion deseade: "))
+    if opcionEntrenamineto == 1:
+        i = 0
+        while oros[i] >= 10:
+
+    elif opcionEntrenamineto == 2:
+
+    elif opcionEntrenamineto == 3:
+
+    elif opcionEntrenamineto == 4:
+
+    elif opcionEntrenamineto == 5:
